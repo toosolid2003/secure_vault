6fa04495694bc9dafd8a23fca1e5cf7a6e0f6e00
toosolid2003 <toosolid2003@yahoo.fr>
2025-05-23 23:00:37 +0200

    First version

---
+++ contracts/secure_vault.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+// Uncomment this line to use console.log
+import "hardhat/console.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SecureVault is Ownable, ReentrancyGuard {
+    
+    // Define events
+    event Deposit(address indexed contributor, uint amount);
+    event Refund(address indexed contributor, uint amount);
+    event Claimed(address indexed hunter, uint amount);
+
+    // Define variables
+    address public hunter;
+    mapping(address => uint256) private balances;
+    uint public deadline;
+
+    // Constructor
+    constructor(address initialOwner) Ownable(initialOwner) {
+        initialOwner = msg.sender;
+        deadline = block.timestamp + 10 days; // Set a deadline of 10 days from deployment
+    }
+    
+    // Anyone can deposit ETH
+    function desposit() external payable {
+        require(msg.value > 0, "Must contain ethers");
+        require(msg.value <= 2 ether, "Deposit value too high");
+
+        balances[msg.sender] += msg.value;
+
+        emit Deposit(msg.sender, msg.value);
+    }
+    // Anyone can check their balance
+    function checkBalance() public view returns (uint256) {
+        return balances[msg.sender];
+    }
+    // Anyone can check the contract balance
+    function checkContractBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+    // Anyone can check the deadline
+    function checkDeadline() public view returns (uint256) {
+        return deadline;
+    }
+
+    // Owner can assign a hunter who can claim the bounty
+    function assign_hunter(address _hunter) onlyOwner public {
+        require(_hunter != address(0), "Invalid hunter address");
+        hunter = _hunter;
+    }
+
+    // The bounty must be claimed before a deadline
+    function claim_bounty() external nonReentrant {
+        require(msg.sender == hunter, "Only the hunter can claim the bounty");
+        require(block.timestamp < deadline, "Deadline has expired");
+
+        uint256 contractBalance = address(this).balance;
+        (bool success,) = payable(hunter).call{value: contractBalance}("");
+        require(success, "Bounty claimed failed");
+
+        emit Deposit(hunter, contractBalance);
+
+        // Reset the hunter address to prevent double claiming
+        hunter = address(0);
+    }
+    // After the deadline, contributors can request a refund
+    function refund() public  {
+        require(balances[msg.sender] > 0, "No money to refund");
+        require(block.timestamp > deadline, "Deadline has not expired");
+
+        uint256 amountToRefund = balances[msg.sender];
+        require(amountToRefund > 0,"Nothing to refund");
+
+        balances[msg.sender] = 0;
+
+        (bool success,) = payable(msg.sender).call{value: amountToRefund}("");
+        require(success, "Refund failed");
+
+        emit Refund(msg.sender, amountToRefund);
+    }
+}
