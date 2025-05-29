ba8bb60808cb38bcf0429974537ce10f1c4cafaf
toosolid2003 <toosolid2003@yahoo.fr>
2025-05-29 16:23:07 +0200

    Deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ignition";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+console.log("RPC_URL:", process.env.RPC_URL);
+console.log("PRIVATE_KEY:", process.env.PRIVATE_KEY);
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sepolia:  {
+      url: process.env.RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [`0x${process.env.PRIVATE_KEY}`] : [],
+    },
+  },
+  gasReporter:  {
+    enabled: true,
+    currency: "ETH",
+  }
 };
 
 export default config;
