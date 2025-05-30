cbe4b8fedd9310ccd86283ea31945091ad0f74f1
toosolid2003 <toosolid2003@yahoo.fr>
2025-05-30 15:31:53 +0200

    Working frontend

---
+++ secure-vault-frontend/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState  } from 'react';
+import { BrowserProvider, Contract } from "ethers";
+import './App.css'
+import SecureVault from '../../artifacts/contracts/secure_vault.sol/SecureVault.json';
+
+
+export const abi = SecureVault.abi;
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+function App() {
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [contract, setContract] = useState(null);
+
+  const connectWallet = async () => {
+    try{
+    if(!window.ethereum) return alert("Install MetaMask");
+
+    const address = await window.ethereum.request({ method: "eth_requestAccounts"});
+    setWalletAddress(address);
+
+    const provider = new BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    const contractInstance = new Contract(contractAddress, SecureVault.abi, signer);
+    console.log("Contract connected:", contractInstance);
+    console.log("Signer:", signer);
+
+    setContract(contractInstance);
+    }
+    catch (err) {
+      console.error("Wallet connection failed:", err);
+    }
+
+  }
+  const readContract = async () => {
+    if (!contract) return alert("Contract not connected");
+    const value = await contract.checkBalance();
+    setMessage(value.toString());
+  }
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>SecureVault Frontend</h1>
+      <button onClick={connectWallet}>
+        {walletAddress ? `Connected: ${walletAddress}` : "Connect Wallet"}
+      </button>
+
+      <div style={{ marginTop: "1rem" }}>
+        <button onClick={readContract}>Read From Contract</button>
+        <p>Response: {message}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
